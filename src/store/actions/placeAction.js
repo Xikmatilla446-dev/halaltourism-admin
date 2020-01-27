194713df078bb635073e78adc1d1f77d4bdfa538
Xikmatilla44 <xikmatilla327@gmail.com>
2020-01-27 14:15:15 +0500

    first commit

---
+++ src/store/actions/placeAction.js
@@ -0,0 +1,10 @@
+import * as actionsTypes from './actionsTypes'
+import axios from 'axios';
+
+
+export const plaseStart =() =>{
+
+    return{
+
+    };
+};
---
+++ src/store/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import    loginStore from './LoginStore/loginStore'
+
+export default combineReducers({
+    loginStore
+});
